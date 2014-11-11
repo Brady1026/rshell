0c248d7f5d1624fa816fdbc1bec177897d094dca
Brady Leong <bleon001@ucr.edu>
2014-11-10 16:51:06 -0800

    increased number of arguments supported

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,20 @@ using namespace std;
 int main()
 {
     string input;
-    char *test[4];
     cout << "$ ";
     getline(cin, input);
-    char cinput[40];
+    char *cinput = new char[input.size()];
     char delimit[] = " ";
     strcpy(cinput, input.c_str());
     strtok(cinput, "#");
-    test[0] = strtok(cinput, delimit);
-    test[1] = strtok(NULL, delimit);
-    test[2] = strtok(NULL, delimit);
-    test[3] = '\0';
-    int returnval = execvp(cinput, test);
+    char **args = new char*[input.size()];
+    char *temp = strtok(cinput, delimit);
+    for(unsigned i = 0; temp != NULL; i++)
+    {
+        args[i] = temp;
+        temp = strtok(NULL, delimit);
+    }
+    int returnval = execvp(args[0], args);
     if (returnval == -1) 
     {
         perror("execvp()");
